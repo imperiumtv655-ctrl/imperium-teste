15cf51634f02af1ab9c4f24d1d7efbbc572efdd6
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-05-31 19:00:02 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+
+const testeRoute = require('./routes/teste');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.use('/api/teste', testeRoute);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
