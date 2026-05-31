59792974e7b1310fe9c09e2c2d5186467e0d49a1
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-05-31 19:00:51 -0300

    Update evolution.js

--- services/evolution.js
+++ services/evolution.js
@@ -1,1 +1,22 @@
 
+const axios = require('axios');
+
+async function enviarWhatsapp(numero,mensagem){
+
+    const url = `${process.env.EVOLUTION_URL}/message/sendText/${process.env.EVOLUTION_INSTANCE}`;
+
+    await axios.post(
+        url,
+        {
+            number: numero,
+            text: mensagem
+        },
+        {
+            headers:{
+                apikey: process.env.EVOLUTION_APIKEY
+            }
+        }
+    );
+}
+
+module.exports = { enviarWhatsapp };
