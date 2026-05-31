c5aeba348da53e12bc5da9ce01e514bc160bec06
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-05-31 19:00:37 -0300

    Update netplay.js

--- services/netplay.js
+++ services/netplay.js
@@ -1,1 +1,20 @@
 
+const axios = require('axios');
+
+async function gerarTeste(nome, telefone, tipoTeste) {
+
+    let url = process.env.NETPLAY_SEM_ADULTO;
+
+    if(tipoTeste === 'adulto'){
+        url = process.env.NETPLAY_COM_ADULTO;
+    }
+
+    const resposta = await axios.post(url,{
+        senderName: nome,
+        senderPhone: telefone
+    });
+
+    return resposta.data;
+}
+
+module.exports = { gerarTeste };
