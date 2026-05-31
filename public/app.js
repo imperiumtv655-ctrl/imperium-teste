7aa7c13976f14d9ec760efe474da05c9d1c3df72
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-05-31 19:25:10 -0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,1 +1,34 @@
 
+const form = document.getElementById('formTeste');
+
+form.addEventListener('submit', async (e) => {
+
+  e.preventDefault();
+
+  const nomeCliente =
+    document.getElementById('nomeCliente').value;
+
+  const whatsapp =
+    document.getElementById('whatsapp').value;
+
+  const tipoTeste =
+    document.getElementById('tipoTeste').value;
+
+  const resposta = await fetch('/api/teste', {
+    method: 'POST',
+    headers: {
+      'Content-Type':'application/json'
+    },
+    body: JSON.stringify({
+      nomeCliente,
+      whatsapp,
+      tipoTeste
+    })
+  });
+
+  const dados = await resposta.json();
+
+  document.getElementById('resultado').innerHTML = `
+    <p>${dados.mensagem}</p>
+  `;
+});
