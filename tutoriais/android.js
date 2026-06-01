79555dac956c22e0bc3f0c06f9d3de2d20f25403
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-06-01 13:55:09 -0300

    Update android.js

--- tutoriais/android.js
+++ tutoriais/android.js
@@ -1,1 +1,24 @@
 
+const androidTvTutorial = {
+  id: 'androidtv',
+  dispositivo: 'Android TV / Google TV',
+  icone: '📺',
+  app: 'NetPlay',
+
+  modelos: [
+    'TCL',
+    'Philips',
+    'Philco',
+    'AOC',
+    'Semp',
+    'Sony',
+    'Panasonic',
+    'Toshiba',
+    'Xiaomi TV',
+    'Google TV'
+  ],
+
+  descricao: 'Use esta opção se sua TV tiver Play Store, Android TV ou Google TV.'
+};
+
+export default androidTvTutorial;
