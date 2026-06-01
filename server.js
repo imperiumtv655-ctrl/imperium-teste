c8444ded3b3f718f29bc28d878ebc72d9ecd9df0
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-06-01 14:00:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const testeRoute = require('./routes/teste');
+const tutorialRoute = require('./routes/tutorial');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/teste', testeRoute);
+app.use('/api/tutorial', tutorialRoute);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3000;
 
