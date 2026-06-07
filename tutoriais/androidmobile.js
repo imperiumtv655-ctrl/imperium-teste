bfea9a6b5d4e57930d79dfeb6517e760b7ca8f31
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-06-07 08:02:10 -0300

    Update androidmobile.js

--- tutoriais/androidmobile.js
+++ tutoriais/androidmobile.js
@@ -11,24 +11,28 @@ module.exports = {
       imagem: 'imagens/tutorial/androidmobile/passo1.png',
       linkPlayStore: 'https://play.google.com/store/apps/details?id=com.ib.player&hl=pt'
     },
+
     {
       titulo: 'Passo 2',
-      texto: 'Clique em Instalar e aguarde o download terminar.',
+      texto: 'Na Google Play Store clique em Instalar e aguarde o download terminar.',
       imagem: 'imagens/tutorial/androidmobile/passo2.png'
     },
+
     {
       titulo: 'Passo 3',
-      texto: 'Após instalar, clique em Abrir.',
+      texto: 'Após a instalação terminar, clique em Abrir para iniciar o aplicativo.',
       imagem: 'imagens/tutorial/androidmobile/passo3.png'
     },
+
     {
       titulo: 'Passo 4',
-      texto: 'Localize o MAC Address e a Device Key exibidos pelo aplicativo.',
+      texto: 'Ao abrir o aplicativo você verá a tela inicial contendo as informações do dispositivo.',
       imagem: 'imagens/tutorial/androidmobile/passo4.png'
     },
+
     {
       titulo: 'Passo 5',
-      texto: 'Anote o MAC e a KEY. Você irá informar esses dados para ativar seu teste.',
+      texto: 'Localize o MAC Address e a Device Key (KEY). COPIE E COLE exatamente como aparecem dentro do aplicativo para evitar erros na ativação.',
       imagem: 'imagens/tutorial/androidmobile/passo5.png'
     }
   ],
