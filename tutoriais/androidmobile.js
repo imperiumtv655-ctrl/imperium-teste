c71346f997c39cb6fe4278d15eb40d3128a59a6d
imperiumtv655-ctrl <imperiumtv655@gmail.com>
2026-06-07 07:36:02 -0300

    Create androidmobile.js

---
+++ tutoriais/androidmobile.js
@@ -0,0 +1,37 @@
+module.exports = {
+  id: 'androidmobile',
+  dispositivo: 'Android',
+  icone: '📱',
+  app: 'IB Player',
+
+  passos: [
+    {
+      titulo: 'Passo 1',
+      texto: 'Clique no botão abaixo para abrir diretamente o IB Player na Google Play Store.',
+      imagem: 'imagens/tutorial/androidmobile/passo1.png',
+      linkPlayStore: 'https://play.google.com/store/apps/details?id=com.ib.player&hl=pt'
+    },
+    {
+      titulo: 'Passo 2',
+      texto: 'Clique em Instalar e aguarde o download terminar.',
+      imagem: 'imagens/tutorial/androidmobile/passo2.png'
+    },
+    {
+      titulo: 'Passo 3',
+      texto: 'Após instalar, clique em Abrir.',
+      imagem: 'imagens/tutorial/androidmobile/passo3.png'
+    },
+    {
+      titulo: 'Passo 4',
+      texto: 'Localize o MAC Address e a Device Key exibidos pelo aplicativo.',
+      imagem: 'imagens/tutorial/androidmobile/passo4.png'
+    },
+    {
+      titulo: 'Passo 5',
+      texto: 'Anote o MAC e a KEY. Você irá informar esses dados para ativar seu teste.',
+      imagem: 'imagens/tutorial/androidmobile/passo5.png'
+    }
+  ],
+
+  regiaoPassos: []
+};
